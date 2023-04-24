6bce386ac79d7b87ac4c3d99627d491af7aa5e6c
Ken Mueller <kenmueller0@gmail.com>
2023-04-24 09:16:27 -0700

    cache

--- lib/getTodos.ts
+++ lib/getTodos.ts
@@ -1,14 +1,14 @@
 import errorFromResponse from '@/lib/error/fromResponse'
 import getUrl from '@/lib/getUrl'
+import { cache } from 'react'
 
-const getTodos = async () => {
+const getTodos = cache(async () => {
 	const url = getUrl()
 
 	const response = await fetch(`${url.origin}/api/todos`)
-
 	if (!response.ok) throw await errorFromResponse(response)
 
 	return (await response.json()) as string[]
-}
+})
 
 export default getTodos
