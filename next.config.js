7e2c1650ba6602fe16cff9620f706df50db278b7
Ken Mueller <kenmueller0@gmail.com>
2023-04-23 17:24:12 -0700

    removed sass options and updated deps

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const { join } = require('path')
-
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
 	swcMinify: true,
 	experimental: {
 		appDir: true
-	},
-	sassOptions: {
-		includePaths: [join(__dirname, 'styles')]
 	}
 }
 
